8a3577e7c207145aa2fcc687547c44d0dbbfaa73
namansandhu <sandhupehluwala@gmail.com>
2016-10-30 09:43:47 -0400

    Add files via upload

---
+++ Assets/Scripts/LevelManage.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManage : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene (scene);
+
+    }
+}
